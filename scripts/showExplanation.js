7772a4db099a7c57e5b849b871a623867aad5887
BazzoFX <bazzofx@outlook.com>
2025-05-26 22:01:36 +0100

    initial project

---
+++ scripts/showExplanation.js
@@ -0,0 +1,6 @@
+  function showExplanation(){
+      // Show the explanation section
+  document.getElementById('explanation').style.display = 'block';
+  // Show the paragraph immediately after the h2 with class "text"
+  document.querySelector('#explanation + p.text').style.display = 'block';
+  }
