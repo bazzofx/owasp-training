ad5145beb0cdaa888465c4cfe8072d074e624e14
BazzoFX <bazzofx@outlook.com>
2025-05-27 00:34:47 +0100

    updated version 2.0

--- scripts/showExplanation.js
+++ scripts/showExplanation.js
@@ -2,5 +2,6 @@
       // Show the explanation section
   document.getElementById('explanation').style.display = 'block';
   // Show the paragraph immediately after the h2 with class "text"
-  document.querySelector('#explanation + p.text').style.display = 'block';
+  // document.querySelector('#explanation + p.text').style.display = 'block';
+  document.querySelector('#explanation').style.display = 'block';
   }
